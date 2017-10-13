79d6822ed3fb29a30573f7476be45b8f4d18eaa3
gabrijela <gabigavric@gmail.com>
2017-10-13 15:26:20 -0700

    karma init wont work says command not found i have no clue where teh error is so i am going to start from scratch

--- build/js/app.js
+++ build/js/app.js
@@ -1,18 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// import {Name} from './../js/age-checker.js';
-//
-// $(document).ready(function(){
-//   $('.haiku').submit(function(event){
-//     event.preventDefault();
-//     var line1 = $('#line1').val();
-//     $('#entries').text(line1);
-//     newHaiku = new Haiku("haiku", line1)
-//     if (newHaiku.vowelCount() === 17) {
-//       $('.yeshaiku').show();
-//     } else {
-//       $('.nohaiku').show();
-//     }
-//   });
-// });
+
 
 },{}]},{},[1]);
