3b421aa996ca04d541f88eb0842ad9c47c7ca03b
gabrijela <gabigavric@gmail.com>
2017-10-13 16:38:43 -0700

    include constructors for taking in age and birthday and convert time in years to time in seconds

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,12 @@
+export class Calculator {
 
+  constructor(age, dateEntered) {
+    this.age = age;
+    this.dateEntered = dateEntered;
+  }
+
+  checkAgeInSeconds(age) {
+    const timeCount = 31536000;
+    let ageInSeconds = age * secondsInYear;
+    return ageInSeconds;
+  }
