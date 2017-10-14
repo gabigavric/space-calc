7fa1810c49a87a33ca1329ba83c8ffe4af0f007f
gabrijela <gabigavric@gmail.com>
2017-10-13 17:22:00 -0700

    add math function to subtract the inputted age in years from the current year to check the users age

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,22 @@
 export class Calculator {
 
-  constructor(age, dateEntered) {
+  constructor(age, birthday, currentDate, region) {
     this.age = age;
-    this.dateEntered = dateEntered;
+    this.birthday = birthday;
+    this.curretDate = currentDate;
+    this.region = region:
   }
 
   checkAgeInSeconds(age) {
     const timeCount = 31536000;
     let ageInSeconds = age * secondsInYear;
     return ageInSeconds;
+  }
+
+  findAge(date){
+    let currentDate = this.currendDate.getTime(); //10-13-17
+    let birthday = this.birthday.getTime(); // 12-25-1994
+    let yearsOld = currentDate.getFullYear() - birthday.getFullYear(); //2017-1994
+    let difference = Math.floor(yearsOld / 1000);
+    return difference;
   }
