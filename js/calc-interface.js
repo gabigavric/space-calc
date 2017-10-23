b49a1b62873efc29515bf48b86daf2255432a36a
gabrijela <gabigavric@gmail.com>
2017-10-23 00:24:43 -0700

    fix the erros that popped up from expectancy function now it will return age expectancy according to region and it will change on each planet too now

--- js/calc-interface.js
+++ js/calc-interface.js
@@ -2,23 +2,34 @@ import {Calculator} from './../js/calc.js';
   $(function() {
   $("#calculatorForm").submit(function(event) {
     event.preventDefault();
-  
+
     let birthday = new Date($('#birthday').val());
     let currentDate = new Date();
+    console.info("birthday:" + birthday)
     let continent = $("#continent").val();
+    console.info("continent:"+continent);
 
-    let age = new Calculator(birthday, currentDate);
-    let earthAgeSeconds = age.checkAgeInSeconds();
+    let calc = new Calculator(birthday, currentDate, continent);
+    let ageSeconds = calc.checkAgeInSeconds();
+    let expectedSeconds = calc.getLifeExpectancy();
 
-    let mars = age.mars(earthAgeSeconds);
-    let mercury = age.mercury(earthAgeSeconds);
-    let venus = age.venus(earthAgeSeconds);
-    let jupiter = age.jupiter(earthAgeSeconds);
 
-    $("#output").append('<li>' + "Earth: " + earthAgeSeconds + '<br>');
-    $("#output").append('<li>' + "Mercury: " + mars +'<br>');
-    $("#output").append('<li>' + "Venus: " + mercury +'<br>');
-    $("#output").append('<li>' + "Mars: " + venus +'<br>');
-    $("#output").append('<li>' + "Jupiter: " + jupiter +'</li>');
+
+    $("#output").html('');
+    $("#output").append('<li>'
+            + 'Earth current age' + calc.earth(ageSeconds)
+            +' earth max age:'+calc.earth(expectedSeconds) +' </li>');
+    $("#output").append('<li>'
+            + 'Mars current age: '+  calc.mars(ageSeconds)
+            +' Mars max age:'+calc.mars(expectedSeconds) +' </li>');
+    $("#output").append('<li>'
+            + 'Venus current age: ' + calc.venus(ageSeconds)
+            +' Venus max age:'      +calc.venus(expectedSeconds) +' </li>');
+    $("#output").append('<li>'
+            + 'Mercury current age: '+ calc.mercury(ageSeconds)
+            + ' Mercury max age:'  + calc.mercury(expectedSeconds) + '  </li>');
+    $("#output").append('<li>'
+            + 'Jupiter: ' + calc.jupiter(ageSeconds)
+            + ' Jupiter max age:'  + calc.jupiter(expectedSeconds) + '  </li>');
   });
 });
--- js/calc.js
+++ js/calc.js
@@ -1,14 +1,10 @@
 export class Calculator {
-  constructor(birthday, currentDate) {
+
+  constructor(birthday, currentDate, continent) {
     this.birthday = birthday;
     this.currentDate = currentDate;
-    this.expectancy = 0;
-  }
-
-  constructorB(birthday, currentDate, continent) {
-    super.birthday = birthday;
-    super.currentDate = currentDate;
     this.continent = continent;
+
   }
 
   checkAgeInSeconds() {
@@ -19,52 +15,56 @@ export class Calculator {
     return secondsAge;
   }
 
+
+  getLifeExpectancy() {
+      let continentAge = 80;
+
+      if(this.continent === "northAmerica") {
+        continentAge = 75;
+      } else if (this.continent === "southAmerica") {
+        continentAge = 65;
+      } else if (this.continent === "europe") {
+        continentAge = 80;
+      } else if (this.continent === "africa") {
+        continentAge = 60;
+      } else if (this.continent === "asia") {
+        continentAge = 85;
+      } else if (this.continent === "australia") {
+        continentAge = 75;
+      } else {
+        continentAge = 80;
+      }
+      console.info("age seconds:" + this.checkAgeInSeconds()
+                  + " continentAge:" + continentAge);
+      //you are old already don't get to live that much longer
+      if(this.checkAgeInSeconds() > continentAge*31536000){
+
+            continentAge = continentAge+10;
+      }
+    return continentAge*31536000;
+  }
+  planetAge(secondsAge, planetDeviation){
+    let secs =  secondsAge /  (31536000 * planetDeviation);
+    return parseFloat(secs.toFixed(2));
+  }
+
+  earth(secondsAge) {
+    return this.planetAge(secondsAge, 1);
+  }
+
   mercury(secondsAge) {
-     const earth = 31536000;
-     let input = secondsAge / (earth * .24);
-     let age = parseFloat(input.toFixed(2));
-     return age;
+     return this.planetAge(secondsAge, .24);
    }
 
    venus(secondsAge) {
-     const earth = 31536000;
-     let input = secondsAge / (earth * .62);
-     let age = parseFloat(input.toFixed(2));
-     return age;
+     return this.planetAge(secondsAge, .62);
    }
 
    mars(secondsAge) {
-     const earth = 31536000;
-     let input = secondsAge / (earth * 1.88);
-     let age = parseFloat(input.toFixed(2));
-     return age;
+     return this.planetAge(secondsAge, 1.88);
    }
 
    jupiter(secondsAge) {
-     const earth = 31536000;
-     let input = secondsAge / (earth * 11.86);
-     let age = parseFloat(input.toFixed(2));
-     return age;
-   }
-
-   continent() {
-   let continentAge = 0;
-   age = number(document.getElementById("age").value);
-   if(continent === "northAmerica") {
-     continentAge = 75;
-   } else if (continent === "southAmerica") {
-     continentAge = 65;
-   } else if (continent === "europe") {
-     continentAge = 80;
-   } else if (continent === "africa") {
-     continentAge = 60;
-   } else if (continent === "asia") {
-     continentAge = 85;
-   } else if (continent === "australia") {
-     continentAge = 75;
-   } else {
-     continentAge = 80;
-   } return continentAge;
-
+     return this.planetAge(secondsAge, 11.86);
    }
   }
